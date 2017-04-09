172069066823429c3d280655cf5ba603efc1e983
acer22969 <acer22969@gmail.com>
2017-04-09 21:24:48 +0800

    lab6

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using java.io;
+using System.Text.RegularExpressions;
+using opennlp.tools.sentdetect;
+
+namespace lab6
+{
+    class Program
+    {
+        static void Sent(string src)
+        {
+            StreamReader input = new StreamReader(src);
+            StreamWriter output = new StreamWriter(Regex.Replace(src, "(.*).txt", "$1_sent.txt"));
+            while (input.Peek() != -1)
+            {
+                InputStream modelIn = new FileInputStream(@"..\..\..\en-sent.bin");
+                SentenceModel smodel = new SentenceModel(modelIn);
+                SentenceDetector detector = new SentenceDetectorME(smodel);
+                string[] sents = detector.sentDetect(input.ReadLine());
+                foreach (string sent in sents)
+                    output.Write(sent + " ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            foreach (string src in Directory.GetFiles(@"..\..\..\DataSet\"))
+            {
+                Sent(src);
+            }
+        }
+    }
+}
